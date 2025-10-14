91c24a94f003cb595a1e73d28e32d86a3eac9d68
Alec Croue <alec.croue@gmail.com>
2025-10-14 14:59:52 +0200

    added cleaner writing to print_bits_uint8t

--- penelope_tools.c
+++ penelope_tools.c
@@ -6,7 +6,7 @@
 void	p_print_bits_uint8(t_penelope_log_level log_level, uint8_t byte, char const *description, bool print_value)
 {
 	if (print_value == true)
-		p_print_level(log_level, "%s0x%08x", description, byte);
+		p_print_level(log_level, "%s0x%08x =", description, byte);
 	p_print_level(log_level, "0b%d%d%d%d%d%d%d%d\n",
 		(byte >> 7) & 1, (byte >> 6) & 1, (byte >> 5) & 1, (byte >> 4) & 1,
 		(byte >> 3) & 1, (byte >> 2) & 1, (byte >> 1) & 1, (byte & 1));
